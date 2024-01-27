e281142ff544671815a4845badea23238cac3017
toke <54713809+syo-tokeshi@users.noreply.github.com>
2024-01-27 11:08:56 +0900

    eslintを導入して、vueのおすすめする構文解析をできるようにした

---
+++ .eslintrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/vue3-recommended",
+    "@vue/eslint-config-typescript/recommended"
+  ],
+  rules: {
+    "no-redeclare": "off"
+  }
+};
+
